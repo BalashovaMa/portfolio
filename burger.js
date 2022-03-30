a0995ff5b1944c9c25133141e9e2bef2b1099e19
Balashova Maryna <balashova.m.nd@gmail.com>
2022-03-30 06:33:21 +0300

    feat: add project

---
+++ burger.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+    $('.menu-burger_header').click(function () {
+        $('.menu-burger_header').toggleClass('open-menu');
+        $('.header_nav').toggleClass('open-menu');
+
+    });
+});
+$(document).ready(function () {
+    $('.header_nav_a').click(function () {
+        $('.header_nav').removeClass('open-menu');
+        $('.menu-burger_header').removeClass('open-menu');
+
+
+    });
+});
+
+
+
+
+
+
+
